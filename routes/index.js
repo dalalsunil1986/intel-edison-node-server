87737695303c543645cb477e09170568c8346aea
latif sebbane <latifsebbane@latifs-MacBook-Pro.local>
2017-04-18 22:43:53 -0700

    Required the library and turn on LED

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var mraa = require('mraa');
 var router = express.Router();
+
+// Set the Gpio and the direction
+var led = new mraa.Gpio(12, true, true);
+led.dir(mraa.DIR_OUT);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //Light up the Led on 14
+  led.write(1);
+
+  //Turn it off after 500ms
+  setTimeout(function() {
+    led.write(0);
+  }, 500);
+  
+  res.render('index', { title: 'Edison + NodeJS' });
 });
 
 module.exports = router;
